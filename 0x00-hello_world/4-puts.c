476192aaa6a895ddead847d1c984d037b698f7a6
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-13 20:49:35 +0000

    Add: puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints specific string
+ *
+ * Return: 0 everytime
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
