a27756009a681d818675d22b60b9b7d571104581
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-13 20:59:02 +0000

    Add: printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints specific string
+ *
+ * Return: 0 everytime
+ */
+
+int main(void)
+{
+	printf("%s\n" "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
