e6a42e69d2a2b9573430549f2085d76bd4475c92
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-15 01:29:34 +0000

    Add: print digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets
+ *
+ * Description: This prints alphabets in lower case
+ *
+ * Return: 0 everytime
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0'+i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
