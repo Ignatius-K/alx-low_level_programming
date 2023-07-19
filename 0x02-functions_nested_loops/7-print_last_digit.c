380c7268f71cf98f3aa6baa0c683696cd1898d52
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-19 21:12:44 +0000

    Fix: for min int

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <limits.h>
 
 /**
  * print_last_digit - prints last digit of number
@@ -12,11 +12,20 @@ int print_last_digit(int n)
 {
 
 	int last_digit;
+	
+	if (n == INT_MIN)
+	{
+		last_digit = 8;
+	}
 
-	if (n < 0)
-		n = -n;
+	else
+	{
+		if (n < 0)
+			n = -n;
 
-	last_digit = n % 10;
+		last_digit = n % 10;
+	}
+
 	_putchar('0' + last_digit);
 	return (last_digit);
 
