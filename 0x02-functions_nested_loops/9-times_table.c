5157a81361cee3deca3a50a835b9561346919686
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-19 22:04:24 +0000

    Add: times table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table of 9
+ *
+ * Description: Prints the time table of 9*9
+ *
+ * Return: None
+ */
+
+void times_table(void)
+{
+	
+	int i;
+	int j;
+	int pdt;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j=0; j <= 9; j++)
+		{
+			pdt = i*j;
+			
+			_putchar(32);
+			if (pdt <= 9)
+			{	
+				_putchar(32);
+				_putchar('0' + pdt);
+			}
+
+			else
+			{	
+				_putchar('0' + ((pdt-(pdt%10))/10));
+				_putchar('0' + pdt%10);
+			}
+
+			if (j == 9)
+			{
+				_putchar(36);
+			}
+			else
+			{
+				_putchar(44);
+			}
+
+		}
+
+		_putchar('\n');
+	}
+
+}
