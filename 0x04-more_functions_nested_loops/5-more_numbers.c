64ba11b634894fc8ceeca0d058174a739c4f82f5
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-20 23:33:41 +0000

    Fix: many _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,22 +9,20 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int rem;
 
 	for (i = 0; i <= 10; i++)
 	{
 
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-			{
-				_putchar('0' + j);
-			}
-
-			else
+			if (j >= 9)
 			{
 				_putchar('0' + 1);
-				_putchar('0' + j - 10);
 			}
+
+				rem = j % 10;
+				_putchar('0' + rem);
 		}
 
 
