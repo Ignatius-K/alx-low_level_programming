4f2e38c03a1ec4b6eeea78362c61b9225b9e3bfa
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-20 23:59:11 +0000

    Add: print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print line diagonal
+ * @n: number of slashes to print
+ * Description: if n is less than 1
+ * print new line and stop
+ */
+
+void print_diagonal(int n)
+{
+
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(32);
+			}
+
+			_putchar(92);
+		}
+
+		_putchar('\n');
+	}
+
+}
+
