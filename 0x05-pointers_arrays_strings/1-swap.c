034ee985f6e0f97cfc5d64ddce09506feeef6356
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-24 21:09:00 +0000

    Add: task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: first integer
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	int swap = *a;
+
+	*a = *b;
+	*b = swap;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 
 void rest_to_98(int *n);
 
-
+void swap_int(int *a, int *b);
 
 
 
