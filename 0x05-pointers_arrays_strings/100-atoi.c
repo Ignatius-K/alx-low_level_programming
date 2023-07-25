35b8bea2706363c5a4aea81dcd68b8bf3423d5f3
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-25 20:33:54 +0000

    Add: convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _atoi - converts string to integer
+ * @s: string to convert
+ * Return: number integer
+ */
+
+int _atoi(char *s)
+{
+	int j, number, first_digit_position, positives, negatives;
+
+	negatives = 0;
+	positives = 0;
+	first_digit_position = -1;
+	for (j = 0; j < strlen(s); j++)
+	{
+		if (s[j] > 47 && s[j] < 58)
+		{
+			number = s[j] - '0';
+			first_digit_position = j;
+			break;
+		}
+	}
+	if (first_digit_position < 0)
+		number = 0;
+	else
+	{
+		for (j = first_digit_position + 1; j < strlen(s); j++)
+		{
+			if (s[j] >= 48 && s[j] <= 57)
+				number = (number * 10) + (s[j] - '0');
+			else
+			{
+				break;
+			}
+		}
+		for (j = 0; j < first_digit_position; j++)
+		{
+			if (s[j] == '-')
+				negatives += 1;
+			else if (s[j] == '+')
+				positives += 1;
+		}
+		if (negatives > positives)
+			number = number * -1;
+	}
+	return (number);
+}
+
+
+
+
