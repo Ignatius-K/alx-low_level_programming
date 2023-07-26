d435ee15ae2f71b996ea62d8f3678c6f2598b355
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-26 21:10:53 +0000

    Fix: return value by diff

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,15 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	int s1_len;
 	int i;
 
-	s1_len = 0;
+	i = 0;
+	while ((s1[i] != '\0') && (s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
 
-	while (s1[s1_len] != '\0')
-		s1_len++;
-
-	for (i = 0; i < (s1_len + 1); i++)
-
-		if (s1[i] > s2[i])
-			return (15);
-
-		else if (s1[i] < s2[i])
-			return (-15);
-
-	return (0);
+	return (s1[i] - s2[i]);
 }
