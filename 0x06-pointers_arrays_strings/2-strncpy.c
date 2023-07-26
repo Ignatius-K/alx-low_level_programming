7440eebd4e2b422551ca78d9176d370aec7d46e1
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-26 20:09:13 +0000

    Add: string to copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/**
+ * _strncpy - copy string
+ * @dest: destination
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to char of copied string
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	
+
 	int i;
 
 	for (i = 0; i < n; i++)
