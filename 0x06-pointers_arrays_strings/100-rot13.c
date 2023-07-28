78b3fc13fd864f3e4bae75489de06880e646c943
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-28 16:59:00 +0000

    Add: string to rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rot13 - convert string to rot13
+ * @str: string to convert
+ *
+ * Return: converted string pointer
+ */
+char *rot13(char *str)
+{
+
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+
+		while ((str[i] > 64 && str[i] < 122) || (str[i] > 96 && str[i] < 123))
+		{
+			if ((str[i] > 96 && str[i] < 110) || (str[i] > 64 && str[i] < 78))
+			{
+				str[i] += 13;
+			}
+
+			else
+			{
+				str[i] -= 13;
+			}
+
+			break;
+
+		}
+
+		i++;
+
+	}
+
+	return (str);
+}
