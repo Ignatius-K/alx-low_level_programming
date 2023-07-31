fb6c8d12e685b0c4e68155fa199008ea7b364d5f
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-31 17:56:39 +0000

    Add: task 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locate char to string
+ * @s: string to check
+ * @c: character to check
+ *
+ * Return: pointer to character or Null
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+
+	return (NULL);
+}
