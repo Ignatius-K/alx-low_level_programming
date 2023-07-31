55723eea02c2dd13def91169a4879894b3ec8abb
Ignatius-K <ignatiuskisekka@gmail.com>
2023-07-31 19:47:52 +0000

    Add: task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - first occur
+ * @s: string
+ * @accept: accepted string
+ *
+ * Return: pointer to string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    while (*s != '\0')
+    {
+        char *current_accept = accept;
+        while (*current_accept != '\0')
+        {
+            if (*s == *current_accept)
+            {
+                return s; 
+            }
+            ++current_accept;
+        }
+        ++s; 
+    }
+
+    return (0);
+}
