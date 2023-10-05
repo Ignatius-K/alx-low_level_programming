eb1c92c03944789c064cd74451b9c3c5f00efc9e
Ignatius <ignatiuskisekka@gmail.com>
2023-10-05 18:57:00 +0000

    add task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints name of program
@@ -9,15 +10,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-
-	int i;
-
-	i = 0;
-	while (argv[0][i] != '\0')
-	{
-		_putchar(argv[0][i]);
-		i++;
-	}
-	_putchar('\n');
-
+	printf("%s\n", argv[0]);
+	return (0);
 }
