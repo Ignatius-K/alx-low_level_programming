2ec66a26102762d0ea0dcef9b483fbeed09253ea
Ignatius <ignatiuskisekka@gmail.com>
2023-10-05 18:47:32 +0000

    add task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * main - prints name of program
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: exit status code
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	int i;
+
+	i = 0;
+	while (argv[0][i] != '\0')
+	{
+		_putchar(argv[0][i]);
+		i++;
+	}
+	_putchar('\n');
+
+	exit(EXIT_SUCCESS);
+}
