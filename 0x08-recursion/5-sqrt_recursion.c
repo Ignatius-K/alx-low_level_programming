2f5e01d295294da642614ec06f0ba5d050ac9cf3
Ignatius <ignatiuskisekka@gmail.com>
2023-10-08 16:33:33 +0000

    square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * is_square - checks whether it is square
@@ -9,11 +10,12 @@
  */
 int is_square(int root, int square)
 {
-	if (root == 1 || root < 1)
-		return (1);
+
 	if (root * root == square)
 		return (root);
-	return (1 * is_square(root - 1, square));
+	if (root * root > square)
+		return (-1);
+	return (is_square(root + 1, square));
 }
 
 /**
@@ -26,7 +28,7 @@ int _sqrt_recursion(int n)
 {
 	int d;
 
-	d = is_square(n, n);
+	d = is_square(1, n);
 	if (d == 1 && n != 1)
 		return (-1);
 	return (d);
