b78ab55e6082d84a299adfb16216597f290c756e
Ignatius <ignatiuskisekka@gmail.com>
2023-10-08 09:45:12 +0000

    Add calloc dup

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _calloc - creates initialized memory
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to created memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	int i, j;
+	char *c_ptrp;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (int)nmemb; i++)
+	{
+		c_ptrp = (char *)(ptr + i * size);
+
+		for (j = 0; j < size; j++)
+			c_ptrp[j] = 0;
+	}
+	return (ptr);
+}
