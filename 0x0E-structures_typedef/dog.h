1aed121a9843a7517dcd6c7a04408c7dd22204f1
Ignatius <ignatiuskisekka@gmail.com>
2023-10-10 20:47:43 +0000

    Add: task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info abt dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+
+	int age;
+
+	char *owner;
+};
+
+
+#endif /* DOG_H */
