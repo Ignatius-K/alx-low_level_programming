cbb3d1d47c21beaa368cf7322956522e2014331b
Ignatius <ignatiuskisekka@gmail.com>
2023-10-12 02:33:37 +0000

    Fix: supplied operation having more than one char

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,8 +22,8 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (argv[2][0] != '/' && argv[2][0] != '*' && argv[2][0] != '+'
-			&& argv[2][0] != '-' && argv[2][0] != '%')
+	if ((argv[2][0] != '/' && argv[2][0] != '*' && argv[2][0] != '+'
+			&& argv[2][0] != '-' && argv[2][0] != '%') || argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
