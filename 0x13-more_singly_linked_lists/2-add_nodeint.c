a96c95de4689f40a815da971f7f2fad2cbc2894f
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-02-05 21:41:31 +0300

    Fix: unhandled malloc NULL return

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,16 +12,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newNode;
 
 	newNode = malloc(sizeof(listint_t));
-	newNode->n = n;
-	if (head == NULL)
-	{
-		newNode->next = NULL;
-	}
-	else
-	{
-		newNode->next = *head;
-	}
+	if (newNode == NULL)
+		return (NULL);
 
+	newNode->n = n;
+	newNode->next = *head;
 	*head = newNode;
+
 	return (newNode);
 }
